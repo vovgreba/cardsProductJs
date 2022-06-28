86db67961f1f7df491bc5c9be1a0595c596b74f6
vovgreba <49303544+vovgreba@users.noreply.github.com>
2022-06-28 17:16:12 +0300

    added new filter

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 const showProductBtn = document.querySelector('.show_product');
 const cardsProducts = document.querySelector('.cards_products');
 const cardsButtons = document.querySelector('.cards_buttons');
-const sortPrice = document.querySelector('.sort-price');
+const sortFilter = document.querySelector('.sort-filter');
+const sort = document.querySelector('.sort');
 
 
 
@@ -33,6 +34,14 @@ const productsArray = [
   },
   {
     image: 'images/bike 1.jpg',
+    name: 'bike-medium-sport ',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ea, voluptates numquam id dolorem, nostrum dolores accusamus harum temporibus veritatis.',
+    rating: '***',
+    availability: 70,
+    price: 2350,
+  },
+  {
+    image: 'images/bike 1.jpg',
     name: 'bike sport-pro',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ea, voluptates numquam id dolorem, nostrum dolores accusamus harum temporibus veritatis.',
     rating: '*****',
@@ -52,8 +61,8 @@ const productsArray = [
 
 const showProduct = () => {
   
-  const rootElement = document.createElement('div')
-  rootElement.className = 'cards_wrapper'
+  const rootElement = document.createElement('div');
+  rootElement.className = 'cards_wrapper';
 
   productsArray.forEach(el => {
     const div = document.createElement('div');
@@ -68,13 +77,13 @@ const showProduct = () => {
     
     if(el.name) {
 
-      const html = (`<h2 class="card_name">Велосипед взрослый <span>${el.name}</span></h2> `)
+      const html = (`<h2 class="card_name">Велосипед взрослый <span>${el.name}</span></h2> `);
       div.insertAdjacentHTML('beforeend', html);
       
     }
 
     if(el.description) {
-      const html = (`<p class="card_desc">${el.description}</p>`)
+      const html = (`<p class="card_desc">${el.description}</p>`);
       div.insertAdjacentHTML('beforeend', html);
 
     }
@@ -82,65 +91,85 @@ const showProduct = () => {
       const html = (`<div class="card_options">
       <div class="card_rating">rating <span>${el.rating}</span></div>
       <div class="card_availability">availability <span>${el.availability}</span> </div>
-      </div>`)
+      </div>`);
       div.insertAdjacentHTML('beforeend', html);
     }
     if(el.price) {
       const html = (`<div class="card_options">
       <div class="card_price">rating <span>${el.price}$</span></div>
       <button class="card_price-btn" type="button">Купить</button>
-      </div>`)
+      </div>`);
       div.insertAdjacentHTML('beforeend', html);
     }
     rootElement.appendChild(div);
   })
   cardsProducts.appendChild(rootElement);
 }
+
 const checkElementsOnPage = nodeEl => {
   for(let el of nodeEl) {
     if(el.className === 'cards_wrapper') {
-      el.remove()
+      el.remove();
     }
   }
 }
 
-const getSortCardProductByPrice = (event) => {
-  checkElementsOnPage(cardsProducts.children)
-
+const sortArray = (event, key) => {
+  
   if(event === 'От меньшего к большему') {
+    
     productsArray.sort((a,b) => {
-      if(a.price > b.price) {
+      if(a[key] > b[key]) {
         return 1;
       }
-      if(a.price < b.price) {
+      if(a[key] < b[key]) {
         return -1;
       }
     })
   }
+
   if(event === 'От большего к меньшему') {
     productsArray.sort((a,b) => {
-      if(a.price > b.price) {
+      if(a[key] > b[key]) {
         return -1;
       }
-      if(a.price < b.price) {
+      if(a[key] < b[key]) {
         return 1;
       }
+    })
+  }
+}
+
+const getSortCardProduct = (event) => {
+
+  checkElementsOnPage(cardsProducts.children);
+  
+  sort.addEventListener('change', ev => {
+    let value = ev.target.value;
+    getSortCardProductByFilter(event, value);
   })
   
+}
+
+const getSortCardProductByFilter = (event, value) => {
+  
+  checkElementsOnPage(cardsProducts.children);
+
+  for(let el of sort.children) {
+    sortArray(event, el.dataset[value]);
+    
   }
   showProduct();
 }
 
+sortFilter.addEventListener('change', ev => {
+  let value = ev.target.value;
+  getSortCardProduct(value);
 
-
-sortPrice.addEventListener('change', ev => {
-  getSortCardProductByPrice(ev.target.value)
-  
 })
 
 showProductBtn.addEventListener('click', () => {
   showProduct();
-  
 })
 
 
